c32b20a511abbff2f1407a0616d4e375563daa2b
Frankenstone <dampf_acc@yahoo.com>
2014-12-23 11:37:04 +0100

    add tuxtxt.ttf,remove DejaVuLGCSansMono-Bold.ttf_neutrino-release.mk

--- make/neutrino-release.mk
+++ make/neutrino-release.mk
@@ -807,10 +807,13 @@ endif
 	if [ -e $(targetprefix)/usr/share/fonts/micron.ttf ]; then \
 		cp -aR $(targetprefix)/usr/share/fonts/micron.ttf $(prefix)/release/usr/share/fonts; \
 	fi
-	if [ -e $(targetprefix)/usr/share/fonts/DejaVuLGCSansMono-Bold.ttf ]; then \
-		cp -aR $(targetprefix)/usr/share/fonts/DejaVuLGCSansMono-Bold.ttf $(prefix)/release/usr/share/fonts; \
-		ln -s /usr/share/fonts/DejaVuLGCSansMono-Bold.ttf $(prefix)/release/usr/share/fonts/tuxtxt.ttf; \
+	if [ -e $(targetprefix)/usr/share/fonts/tuxtxt.ttf ]; then \
+		cp -aR $(targetprefix)/usr/share/fonts/tuxtxt.ttf $(prefix)/release/usr/share/fonts; \
 	fi
+#	if [ -e $(targetprefix)/usr/share/fonts/DejaVuLGCSansMono-Bold.ttf ]; then \
+#		cp -aR $(targetprefix)/usr/share/fonts/DejaVuLGCSansMono-Bold.ttf $(prefix)/release/usr/share/fonts; \
+#		ln -s /usr/share/fonts/DejaVuLGCSansMono-Bold.ttf $(prefix)/release/usr/share/fonts/tuxtxt.ttf; \
+#	fi
 
 #
 # neutrino
